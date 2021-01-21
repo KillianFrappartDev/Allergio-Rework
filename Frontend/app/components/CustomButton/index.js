15d7a6368f573884d881aa8b89d9fe8c92a3b141
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-21 10:01:20 +0100

    Finished IOS like button

--- Frontend/app/components/CustomButton/index.js
+++ Frontend/app/components/CustomButton/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 const CustomButton = props => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={props.onPress}>
+      <Text style={[styles[props.color], styles.main]}>{props.title}</Text>
+    </TouchableOpacity>
   );
 };
 
--- Frontend/app/components/CustomButton/styles.js
+++ Frontend/app/components/CustomButton/styles.js
@@ -1,5 +1,11 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  main: { fontSize: 18 },
+  container: { paddingVertical: 5, paddingRight: 5 },
+  red: { color: 'red' },
+  blue: { color: 'blue' },
+  green: { color: 'green' }
+});
 
 export default styles;
